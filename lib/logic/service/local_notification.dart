4f9fe02408b925909e96c94c0b1be7c2cff80484
Nishant <nishantyt0@gmail.com>
2023-07-30 20:35:13 +0545

    feat : notification service added

--- lib/logic/service/local_notification.dart
+++ lib/logic/service/local_notification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:nepali_calendar/ui/commons/colors.dart';
 import 'package:nepali_utils/nepali_utils.dart';
 
 class LocalNotificationService {
@@ -7,9 +8,9 @@ class LocalNotificationService {
       FlutterLocalNotificationsPlugin();
 
   static void initialize() {
-    const InitializationSettings initializationSettingsAndroid =
-        InitializationSettings(
-            android: AndroidInitializationSettings('@mipmap/ic_launcher'));
+    InitializationSettings initializationSettingsAndroid =
+        const InitializationSettings(
+            android: AndroidInitializationSettings('ic_launcher'));
     _notificationsPlugin.initialize(
       initializationSettingsAndroid,
       onDidReceiveNotificationResponse: (details) {
@@ -22,19 +23,29 @@ class LocalNotificationService {
     // To display the notification in device
     try {
       final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-      NotificationDetails notificationDetails = const NotificationDetails(
-        android: AndroidNotificationDetails("Channel Id", "Main Channel",
-            groupKey: "gfg",
-            color: Colors.green,
-            importance: Importance.max,
-            // sound: RawResourceAndroidNotificationSound("gfg"),
-            playSound: true,
-            priority: Priority.high),
+      NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+          "Channel Id",
+          "Main Channel",
+          largeIcon: const DrawableResourceAndroidBitmap('drawable/ic_launcher'),
+          category: AndroidNotificationCategory.status,
+          groupKey: "gfg",
+          autoCancel: false,
+          playSound: false,
+          onlyAlertOnce: true,
+          ongoing: true,
+          icon: 'drawable/ic_launcher',
+          color: kBackgroundColor,
+          importance: Importance.max,
+          priority: Priority.high,
+        ),
       );
       await _notificationsPlugin.show(
         id,
-        '${NepaliDateTime.now().day.toString()} ${NepaliDateTime.now().month}',
-        'message.notification?.body',
+        '${NepaliDateTime.now().day.toString()} ${NepaliDateFormat.MMMM().format(
+          NepaliDateTime.now(),
+        )}, ${NepaliDateTime.now().year}',
+        'Opn Miti To See List of Events',
         notificationDetails,
       );
     } catch (e) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:nepali_calendar/logic/service/local_notification.dart';
 
 import 'package:nepali_calendar/ui/main_screen/screens/main_screen.dart';
 import 'package:nepali_calendar/ui/providers/calendar_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:workmanager/workmanager.dart';
+
+import 'logic/service/background_work.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher);
   runApp(const CalendarApp());
 }
 
-class CalendarApp extends StatelessWidget {
+class CalendarApp extends StatefulWidget {
   const CalendarApp({super.key});
+
+  @override
+  State<CalendarApp> createState() => _CalendarAppState();
+}
+
+class _CalendarAppState extends State<CalendarApp> {
+  @override
+  void initState() {
+    super.initState();
+    LocalNotificationService.initialize();
+    LocalNotificationService.display();
+  }
 
   @override
   Widget build(BuildContext context) {
