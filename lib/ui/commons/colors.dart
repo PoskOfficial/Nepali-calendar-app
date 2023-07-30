b681fdba93c06b47a8b96784ec680aecf58ca6a0
Nishant <nishantyt0@gmail.com>
2023-07-30 17:04:21 +0545

    chore: events started

--- lib/ui/commons/colors.dart
+++ lib/ui/commons/colors.dart
@@ -6,3 +6,4 @@ Color kCalendarComponentColor = const Color(0xFF111827);
 Color kCalendarBorderColor = const Color(0xFF1F2937);
 Color kOuterCalendarBorder = const Color(0xFF4B5563);
 Color kForegroundColor = Colors.white;
+Color kButtonColor = Colors.blue;
